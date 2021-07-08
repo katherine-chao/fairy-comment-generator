bf63df44c96780b324a906e3dccbc687ae837a06
Katherine <kat2chao@gmail.com>
2021-07-08 19:13:24 +0000

    fairy comment generator

--- calc.py
+++ calc.py
@@ -1,1 +1,36 @@
 #testing git
+# print("Are you a boy or a girl?")
+# gender = input()
+# genderDictionary = {"boy": "🧑‍💻", "girl": "👩‍💻"}
+# print("Are you a beginner coder?")
+# beginnerCoder = input()
+# if(beginnerCoder == "yes"):
+#     print("✨ im glad you came out of your comfort zone!" + genderDictionary[gender] + " 💞 🧚 Now go back there 🧚 🕳")
+print("Which state are you from?")
+state = input().lower()
+if(state == "texas"):
+    print("don't mess with Texas! 🤠 🧚 💪 Nobody wants to mess with you anyways. 🙄 🦦")
+elif(state == "new jersey"):
+    print("how does it feel to be as irrelevant as the state you live in? 🌾 🐄 🧚‍♂️")
+elif(state == "new york"):
+    print("You're from the city that never sleeps? 🏙 🗽 You should try entering into an eternal slumber 🛌 ✨ 🧚")
+print("Is your camera on?")
+camera = input()
+if(camera == "yes"):
+    print("Nice video! Make sure to turn the camera off next time!! 📹 ✨ 🚫 and keep it that way 🧚‍♀️ ✨")
+elif(camera == "no"):
+    print("Good, keep it that way! 📹 ✨ 🚫")
+print("Are you a computer science major?")
+major = input()
+if(major == "yes"):
+    print("here’s a door for FAANG internships 🤓 🚪 💻 i locked it just for you 🔑 🧚")
+elif(major == "no"):
+    print("This is your calling ✨ 🤑 but I think you should hang up ☎️ 🥰 ‍🧚‍♂️")
+print("Are you traveling this summer?")
+travel = input()
+if(travel == "yes"):
+    print("I hope you’re in Spain 🇪🇸 🛫 🧚 the s is silent 🧚 🥲 ⭐")
+elif(travel == "no"):
+    print("You should try joining the circus 🤡 🎪 🤹‍♀️, I heard it's traveling through town. ✨ 🐘 😍")
+print("")
+print("No matter what the mean fairy said, we still love and value you as a human being. 🥰 🧚  Your worth is indescribable! 😇 💛 Your creations are invaluable! 🪄 🎨")
